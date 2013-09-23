f80698ad80f0f3548c2a8ac53f2558466335f606
MACC RADAR <macc.radar@gmail.com>
2013-09-23 17:08:55 +0200

    eunit test node
    adds eunit test for consistency check node holons

---
+++ apps/modum_core/src/node_test.erl
@@ -0,0 +1,16 @@
+-module(node_test).
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(export_all).
+
+setup() ->
+	file:set_cwd("/research/workspace/Erlang/macc_radar/"),
+	io:format("Current working dir: ~p~n", [file:get_cwd()]),
+	application:start(util),
+	application:start(blackboard),
+	application:start(modum_core),
+	ok.
+	
+consistency_test() ->
+	setup(),
+	?assertMatch({reply,{check_consistency,nodes},true}, gen_server:call(modum_proxy, {check_consistency,nodes})).
